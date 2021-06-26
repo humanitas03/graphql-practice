5d3105a3f3d0f9cea80f94088b011b66811520dd
humanitas03 <dlswp113@gmail.com>
2021-06-26 20:36:26 +0900

    mutation 구현

--- graphql-test-server/index.js
+++ graphql-test-server/index.js
@@ -22,11 +22,28 @@ const typeDefs = gql`
   type Equipment {
     id: String
     used_by: String
+    count: Int
     new_or_used: String
   }
   type Supply {
     id: String
     team: Int
+  }
+  
+  type Mutation {
+      insertEquipment(
+          id: String,
+          used_by: String,
+          count: Int,
+          new_or_used: String
+      ): Equipment
+      editEquipment(
+          id: String,
+          userd_by: String,
+          count: Int,
+          new_or_used: String
+      ):Equipment
+      deleteEquipment(id: String): Equipment
   }
 `
 
@@ -46,6 +63,32 @@ const resolvers = {
         equipments: () => database.equipments,
         supplies: () => database.supplies
 
+    },
+
+    Mutation: {
+        insertEquipment: (parent, args, context, info) => {
+            database.equipments.push(args)
+            return args
+        },
+        editEquipment: (parent, args, context, info) => {
+            return database.equipments.filter((equipment)=>{
+                return equipment.id===args.id
+            }).map((equipment)=>{
+                Object.assign(equipment, args)
+                return equipment
+            })[0]
+        },
+        deleteEquipment: (parent, args, context, info) => {
+            const deleted = database.equipments
+                .filter((equipment) => {
+                    return equipment.id === args.id
+                })[0]
+            database.equipments = database.equipments
+                .filter((equipment) => {
+                    return equipment.id !== args.id
+                })
+            return deleted
+        }
     }
 }
 
